890adfa15492674862f263b8439f113038b4de29
DefenderJohn <1175745126@qq.com>
2024-02-21 16:22:58 -0700

    完成功能，测试得到的结果非常不错

--- Translator_shell.py
+++ Translator_shell.py
@@ -26,7 +26,7 @@ def seperateSubtitleSegment(subtitles:List[Tuple[Tuple[float, float], str]], tra
     for historyIndex in range(max(index - historyCount, 0), index):
         userPrompt = {
             'role':'user',
-            'content': translations[historyIndex][1][0]
+            'content': buildOnePrompt(subtitles=subtitles, index=historyIndex, forwardCount=forwardCount)
         }
         assistanceRespond = {
             'role':'assistant',
@@ -35,11 +35,14 @@ def seperateSubtitleSegment(subtitles:List[Tuple[Tuple[float, float], str]], tra
         history.append(userPrompt)
         history.append(assistanceRespond)
         
-    current = '当前字幕： ' + subtitles[index][1]
-    forward = "作为参考的后续字幕（不翻译）\n"
+    return (history, buildOnePrompt(subtitles=subtitles, index=index, forwardCount=forwardCount))
+
+def buildOnePrompt(subtitles:List[Tuple[Tuple[float, float], str]], index:int, forwardCount:int) -> str:
+    current = '当前字幕：' + subtitles[index][1] + "\n"
+    forward = "作为参考的后续字幕（不翻译）\n" if forwardCount != 0 else ""
     for forwardIndex in range(index, min(forwardCount + index, len(subtitles))):
-        forward += subtitles[forwardIndex][1]
-    return (history, current + forward + "\n翻译当前字幕")
+        forward += subtitles[forwardIndex][1] + "\n"
+    return current + forward + "\n翻译当前字幕"
 
 def translateByChatGLM(history:str, prompt:str, model:AutoModel, tokenizer:AutoTokenizer) -> str:
     stop_stream = False
@@ -53,9 +56,9 @@ def translateByChatGLM(history:str, prompt:str, model:AutoModel, tokenizer:AutoT
             break
         else:
             translateResult += response[current_length:]
-            print(response[current_length:], end="")
+            #print(response[current_length:], end="")
             current_length = len(response)
-    print("")
+    #print("")
     return translateResult
 
 def convertTime(time:float) -> str:
@@ -67,7 +70,7 @@ def convertTime(time:float) -> str:
     return "{:02}:{:02}:{:02},{:03}".format(hours, minutes, seconds, milliseconds)
 
 def writeToFile(content:str, fileName:str):
-    with open(fileName, 'w') as file:
+    with open(fileName, 'w', encoding='UTF-8') as file:
         file.write(content)
 
 def findVideos(directory):
@@ -80,14 +83,60 @@ def findVideos(directory):
     print(f"找到{len(videos)}个视频文件")
     return videos
 
-selectedPath = "..."
-for filePath in tqdm(findVideos(selectedPath), desc="正在翻译全部视频"):
-    translate = False
+batch = False
+batchPath = ""
+videoPath = ""
+translate = True
+bothLanguage = True
+if batch:
+    selectedPath = batchPath
+    for filePath in tqdm(findVideos(selectedPath), desc="正在翻译全部视频"):
+        fileName = filePath.split('.')[0]
+        transcribedSubtitles = transcribeToList(videoPath=filePath, model="large", showText=True)
+        translatedSubtitles = []
+        historyCount = 10
+        forwardCount = 0
+        if translate:
+            print("正在准备ChatGLM模型：")
+            tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm3-6b", trust_remote_code=True)
+            model = AutoModel.from_pretrained("THUDM/chatglm3-6b", trust_remote_code=True).cuda()
+            model = model.eval()
+            print("ChatGLM准备完毕")
+            for index in tqdm(range(len(transcribedSubtitles)), desc="正在执行滑动窗口翻译", position=1):
+                history, prompt = seperateSubtitleSegment(subtitles=transcribedSubtitles, translations=translatedSubtitles, index=index, historyCount=historyCount, forwardCount=forwardCount)
+                translated = translateByChatGLM(history=history, prompt=prompt, model=model, tokenizer=tokenizer)
+                beginTime = transcribedSubtitles[index][0][0]
+                endTime = transcribedSubtitles[index][0][1]
+                original = transcribedSubtitles[index][1]
+                translatedSubtitles.append(((beginTime, endTime),(original, translated)))
+            index = 0
+            result = ""
+            for subtitle in translatedSubtitles:
+                index += 1
+                start = convertTime(subtitle[0][0])
+                end = convertTime(subtitle[0][1])
+                context = subtitle[1][1]
+                subtitleContent = f"{index}\n{start} --> {end}\n{context}\n\n"
+                result += subtitleContent
+            writeToFile(fileName=f"{fileName}.srt", content=result)
+        else:
+            index = 0
+            result = ""
+            for subtitle in transcribedSubtitles:
+                index += 1
+                start = convertTime(subtitle[0][0])
+                end = convertTime(subtitle[0][1])
+                context = subtitle[1]
+                subtitleContent = f"{index}\n{start} --> {end}\n{context}\n\n" if not bothLanguage else f"{index}\n{start} --> {end}\n{context}\n{original}\n\n"
+                result += subtitleContent
+            writeToFile(fileName=f"{fileName}.srt", content=result)
+else:
+    filePath = videoPath
     fileName = filePath.split('.')[0]
-    transcribedSubtitles = transcribeToList(videoPath=filePath, model="large-v2", showText=True)
+    transcribedSubtitles = transcribeToList(videoPath=filePath, model="large", showText=True)
     translatedSubtitles = []
     historyCount = 10
-    forwardCount = 3
+    forwardCount = 0
     if translate:
         print("正在准备ChatGLM模型：")
         tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm3-6b", trust_remote_code=True)
@@ -101,15 +150,25 @@ for filePath in tqdm(findVideos(selectedPath), desc="正在翻译全部视频"):
             endTime = transcribedSubtitles[index][0][1]
             original = transcribedSubtitles[index][1]
             translatedSubtitles.append(((beginTime, endTime),(original, translated)))
-        for item in translatedSubtitles:
-            print(item)
-    index = 0
-    result = ""
-    for subtitle in transcribedSubtitles:
-        index += 1
-        start = convertTime(subtitle[0][0])
-        end = convertTime(subtitle[0][1])
-        context = subtitle[1]
-        subtitleContent = f"{index}\n{start} --> {end}\n{context}\n\n"
-        result += subtitleContent
-    writeToFile(fileName=f"{selectedPath}/{fileName}.srt", content=result)
+        index = 0
+        result = ""
+        for subtitle in translatedSubtitles:
+            index += 1
+            start = convertTime(subtitle[0][0])
+            end = convertTime(subtitle[0][1])
+            original = subtitle[1][0]
+            context = subtitle[1][1]
+            subtitleContent = f"{index}\n{start} --> {end}\n{context}\n\n" if not bothLanguage else f"{index}\n{start} --> {end}\n{context}\n{original}\n\n"
+            result += subtitleContent
+        writeToFile(fileName=f"{fileName}.srt", content=result)
+    else:
+        index = 0
+        result = ""
+        for subtitle in transcribedSubtitles:
+            index += 1
+            start = convertTime(subtitle[0][0])
+            end = convertTime(subtitle[0][1])
+            context = subtitle[1]
+            subtitleContent = f"{index}\n{start} --> {end}\n{context}\n\n"
+            result += subtitleContent
+        writeToFile(fileName=f"{fileName}.srt", content=result)
